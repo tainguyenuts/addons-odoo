7c33cefc8d7a89b27b3bca2e71f7154580534883
tantai251295 <tantai251295@gmail.com>
2024-09-21 23:01:05 +0700

    add tai module

---
+++ tai_custom_module_new/__manifest__.py
@@ -0,0 +1,18 @@
+# __manifest__.py
+{
+    'name': 'My Custom Module',
+    'version': '1.1',
+    'category': 'Custom',
+    'summary': 'Module to add custom fields',
+    'description': 'This module adds custom fields to existing models.',
+    'author': 'Your Name',
+    'website': 'http://yourwebsite.com',
+    'depends': ['base'],  # Specify dependencies here
+    'data': [
+        # 'security/ir.model.access.csv',
+        'views/my_model_views.xml',
+    ],
+    'installable': True,
+    'application': False,
+}
+
---
+++ tai_custom_module_new/models/my_model.py
@@ -0,0 +1,27 @@
+# models/my_model.py
+from odoo import models, fields
+
+class JobStatusOption(models.Model):
+    _name = 'job.status.option'  # New model to store job status options
+    _description = 'Job Status Option'
+
+    name = fields.Char(string='Job Status', required=False)
+
+class InfluenceLevelOption(models.Model):
+    _name = 'influence.level.option'  # Model for Influence Level
+    _description = 'Influence Level Option'
+
+    name = fields.Char(string='Influence Level', required=False)
+
+class ResPartner(models.Model):
+    _inherit = 'res.partner'
+
+    buyer_role = fields.Char(string='Buyer Role (Vai trò bên mua)')
+    influence_level = fields.Many2one('influence.level.option',string='Influence Level (Sức ảnh hưởng)')
+    job_status = fields.Many2one('job.status.option',string='Job Status (Trạng thái công việc))')
+    attitude = fields.Integer(string='Attitude (Thái độ)')
+    flag_alert = fields.Integer(string='Flag Alert (Cờ báo động)')
+    total_score = fields.Integer(string='Total Score (Tổng điểm)')
+    winning = fields.Char(string='Winning', placeholder='Enter Winning')
+    remarks_notes = fields.Char(string='Remarks/Notes (Đánh giá/Lưu ý)')
+    customer_care_assignment = fields.Many2one('res.users', string='Customer Care Assignment (Phân vai chăm sóc)')
